948305e10a4072c8cba78601a36d960695d5b40a
Watnir <137804027+AU0758@users.noreply.github.com>
2024-01-10 18:11:47 +0000

    Added Signature and roulete

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,32 @@
+'use client';
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [CurrSection, setCurrSection] = useState(0)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section id='hero'>
+      <section id='hero' className={CurrSection===0?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
+        <div className='signatureMask absolute text-[#DDD] flex items-center'><h1>O elevador espacial</h1></div>
         <h1>O elevador espacial</h1>
       </section>
-      <section id='cocpit'>
+      <section id='cocpit' className={CurrSection===1?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>O Cocpit</h1>
       </section>
-      <section id='coneccao'>
+      <section id='coneccao' className={CurrSection===2?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>A Coneccao</h1>
       </section>
-      <section id='elevador'>
+      <section id='elevador' className={CurrSection===3?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>O Elevador</h1>
       </section>
-      <section id='hub'>
+      <section id='hub' className={CurrSection===4?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>O Hub</h1>
       </section>
-      <section id='reator'>
+      <section id='reator' className={CurrSection===5?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>O Reator</h1>
       </section>
-      <section id='ancora'>
+      <section id='ancora' className={CurrSection===6?'viseble':'hidden'} onClick={(e) => {setCurrSection(CurrSection === 7 ? 0 : CurrSection+1 )}}>
         <h1>A Ancora</h1>
       </section>
     </main>
